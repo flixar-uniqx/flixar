c7687c862b1839e9400ff466077dbd2d3746300c
flixar-uniqx <flixar.uniqx@gmail.com>
2025-12-23 23:52:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,11 @@ const CONFIG = {
     
     colors: {
         primary: "#00d2ff",
-        bg: "#0f1014",
-        card: "#1b1c22",
-        text: "#f5f5f5"
+        bg: "#0a0a0a",
+        card: "#121212",
+        text: "#ffffff"
     },
 
     footerText: "© 2025 StreamHub. All Rights Reserved.",
-    disclaimer: "We do not host any files on our servers. All content is provided by non-affiliated third parties.",
     latestCount: 12
 };
